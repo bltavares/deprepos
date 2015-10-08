e0f3723a2f9fd9379b68d676dde9c291683b948e
Bruno Tavares <connect+github@bltavares.com>
2015-10-08 01:51:59 -0300

    Fetchs remote updates and reset the working tree with a detached head. Branch names must have the remote prefix to keep in sync

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+extern crate git2;
+
 use std::fs::File;
 use std::io::Read;
+use git2::Repository;
 
-#[derive(Debug)]
 struct Git {
     destination: String,
     reference: String,
-    source: String
+    source: String,
+    repo: Option<git2::Repository>,
 }
 
 trait Dependency {
@@ -13,12 +16,51 @@ trait Dependency {
 
     fn ensure_destination_exists(&self) {
         let destination = self.destination();
-        std::fs::create_dir_all(destination)
-            .ok()
-            .expect(&format!("Could not create the destination: {}", destination));
+        std::fs::create_dir_all(destination).unwrap();
     }
 
-    fn sync(&self);
+    fn sync(&mut self);
+}
+
+impl Git {
+    fn needs_cloning(&self) -> bool {
+        let mut git_location = std::path::PathBuf::from(&self.destination);
+        git_location.push(".git");
+        std::fs::metadata(git_location).is_err()
+    }
+
+    fn clone(&mut self) {
+        let result = git2::Repository::clone(&self.source, &self.destination);
+        self.repo = Some(result.unwrap());
+    }
+
+    fn update(&mut self) {
+        let error_message = &format!("Couldn't find origin remote on {}", &self.destination);
+        let mut remote = self.repo().find_remote("origin").ok().expect(error_message);
+        remote.fetch(&[], None, None).unwrap();
+    }
+
+    fn checkout(&mut self) {
+        let mut checkout_options = git2::build::CheckoutBuilder::new();
+        checkout_options.force();
+
+        let rev_reference = self.reference.clone();
+        let repo = self.repo();
+        let oid = repo.revparse_single(&rev_reference).unwrap();
+
+        repo.set_head_detached(oid.id()).unwrap();
+        repo.reset(&oid, git2::ResetType::Hard, Some(&mut checkout_options)).unwrap();
+    }
+
+    fn repo(&mut self) -> &Repository {
+        match self.repo {
+            Some(ref repo) => repo,
+            None => {
+                self.repo = Repository::open(&self.destination).ok();
+                self.repo()
+            }
+        }
+    }
 }
 
 impl Dependency for Git {
@@ -27,9 +69,17 @@ impl Dependency for Git {
     }
 
 
-    fn sync(&self) {
+    fn sync(&mut self) {
         self.ensure_destination_exists();
-        println!("LOL")
+
+        if self.needs_cloning() {
+            println!("Needs cloning");
+            self.clone();
+        } else {
+            println!("Needs fetch");
+            self.update();
+        }
+        self.checkout();
     }
 }
 
@@ -49,11 +99,13 @@ fn parse_file(s : &str) -> Vec<Box<Dependency>> {
         let reference = String::from(line_words.next().expect("Third argument should be a reference"));
         let source = String::from(line_words.next().expect("Fourth argument should be a source url"));
 
+
         match repo_type {
             "git" => Box::new(Git {
                 destination: destination,
                 reference: reference,
-                source: source
+                source: source,
+                repo: None
             }) as Box<Dependency>,
             _ => {
                 panic!("Can't handle repositories of type: {}", repo_type);
@@ -66,7 +118,7 @@ fn parse_file(s : &str) -> Vec<Box<Dependency>> {
 fn main() {
     let content = dep_content();
     let parsed = parse_file(&content);
-    for dep in parsed {
+    for mut dep in parsed {
         dep.sync();
     }
 }
